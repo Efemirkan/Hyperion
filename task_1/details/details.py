ced8f3cc7149e0a759b39f5c82f842cbd8cb4f8b
Efemirkan <134335079+Efemirkan@users.noreply.github.com>
2023-12-18 23:50:54 +0000

    Add files via upload

---
+++ task_1/details/details.py
@@ -0,0 +1,8 @@
+# Take user's inputs
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+house_number = input("What is your house number? ")
+street_name = input("What is your street name? ")
+
+# Print and display in sentence
+print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_name}.")

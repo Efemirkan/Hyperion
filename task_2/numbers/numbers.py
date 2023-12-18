0d47b1a6049718c953cfe46fb5c71bd21421f7ec
Efemirkan <134335079+Efemirkan@users.noreply.github.com>
2023-12-18 23:53:40 +0000

    Add files via upload

---
+++ task_2/numbers/numbers.py
@@ -0,0 +1,16 @@
+# Take user's inputs and case to integer
+num1 = int(input("Please enter first different integers: "))
+num2 = int(input("Please enter second different integers: "))
+num3 = int(input("Please enter third different integers: "))
+
+# Calculate total of numbers and print to display
+print(num1 + num2 + num3)
+
+# Calculate first integer minus second integer and print to display 
+print(num1 - num2)
+
+# Calculate third number multiply by first number and print to display
+print(num3 * num1)
+
+# Calculate total of numbers then divide by third number and print to display
+print((num1 + num2 + num3) / num3)
---
+++ task_2/replace/replace.py
@@ -0,0 +1,14 @@
+# Initialize Variable
+str = "The!quick!brown!fox!jumps!over!the!lazy!dog."  # To store sentence
+
+# Replace "!" by whitespace and print to display
+str1 = str.replace("!", " ")
+print(str1)
+
+# Capitalise every letter in string and print to display
+str2 = str1.upper()
+print(str2)
+
+# Reverse sentence and print to display
+str3 = str2[::-1]
+print(str3)
